217906934acc7efb8168d049c3badad082afbc84
nikita-gray <fuchko.nikita@gmail.com>
2018-11-03 20:35:10 +0500

    Настроил vagrant на http://back-exchange.test

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 't0ADrH51gIpd8sqHOVLYl_TGvJtSJtxj',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
