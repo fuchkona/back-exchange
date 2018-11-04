f8bab2629935ea6564e46701dd0522f0069b3ed0
Igor Korobeynikov <igrock88@gmail.com>
2018-11-04 13:32:39 +0800

    Добавил настройки бд для деплоя

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_db2b52e0ef2b38b',
+    'username' => 'b890543191508e',
+    'password' => '27467812',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
