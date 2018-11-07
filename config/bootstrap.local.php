cb1b1b06ccfb2f6c0e0ba09af94eca5929d7dd89
Igor Korobeynikov <igrock88@gmail.com>
2018-11-07 19:46:29 +0800

    Добавил локальные конфиги

---
+++ config/bootstrap.local.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igroc
+ * Date: 07.11.2018
+ * Time: 19:26
+ */
---
+++ config/defaults.local.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+            'username' => 'root',
+            'password' => '',
+        ]
+    ],
+];
