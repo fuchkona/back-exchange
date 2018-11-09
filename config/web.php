b73744501ea8c4a8bb219c9e4d945191efd12efc
Igor Korobeynikov <igrock88@gmail.com>
2018-11-09 19:13:13 +0800

    Сделаны настройки для Rest api
    В конфиге включены настройки роутов для rest api

--- config/web.php
+++ config/web.php
@@ -16,6 +16,9 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 't0ADrH51gIpd8sqHOVLYl_TGvJtSJtxj',
             'baseUrl' => '',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -48,6 +51,9 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'api/<c:>'],
             ],
         ],
     ],
