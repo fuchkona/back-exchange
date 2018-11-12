a7f11699d3de71b7aaa4f87603cf1edf019a8a18
Igor Korobeynikov <igrock88@gmail.com>
2018-11-12 23:15:53 +0800

    Добавлен вывод имени пользователя на экран приветствия

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'Time Exchange';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Привествую</h1>
+        <h1>Привествую <?=Yii::$app->user->identity->username ?></h1>
 
         <p class="lead">Вы в админкe Time Exchange</p>
 
