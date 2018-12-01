adf02403a2aa1262dd0dd3041cdacd1a2ddbed09
nikita-gray <fuchko.nikita@gmail.com>
2018-12-02 00:35:36 +0500

    Добавил правила для корректного добавления даты через API по timeshtamp вместо формата Dec 5, 2018

--- modules/api/models/Task.php
+++ modules/api/models/Task.php
@@ -11,6 +11,22 @@ namespace app\modules\api\models;
 
 class Task extends \app\models\Task
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'description', 'owner_id', 'deadline'], 'required'],
+            [['description'], 'string'],
+            [['owner_id', 'worker_id', 'contract_time', 'deadline'], 'integer'],
+            ['deadline', 'default', 'value' => null],
+            [['title'], 'string', 'max' => 300],
+            [['owner_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['owner_id' => 'id']],
+            [['worker_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['worker_id' => 'id']],
+        ];
+    }
+
 
     public function fields()
     {
