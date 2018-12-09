1653955cdbd5434a127e30d83ad0cf62d5c086a4
IlyaKislitsin <ilya.kislitsin@gmail.com>
2018-12-10 02:00:06 +0500

    - исключил возможность создавать несколько запросов на выполнение от одного пользователя и создавать запросы на выполнение своих задач - отредактировал сервисы, чтобы все имели общий принцип вызова

--- modules/api/controllers/RequestController.php
+++ modules/api/controllers/RequestController.php
@@ -10,13 +10,13 @@ namespace app\modules\api\controllers;
 
 
 use app\modules\api\models\Request;
-use app\services\RequestService;
 use Yii;
 use yii\data\ActiveDataProvider;
 use yii\filters\AccessControl;
 use yii\rest\ActiveController;
 use yii\web\ForbiddenHttpException;
 use yii\web\NotFoundHttpException;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 
 class RequestController extends ActiveController
 {
@@ -38,11 +38,27 @@ class RequestController extends ActiveController
                             $request = Request::findOne(['id' => Yii::$app->request->get('request_id')]);
 
                             if (isset($request)){
-                                return RequestService::canDelete($currentUser, $request);
+                                return Yii::$app->requestService->canDelete($currentUser, $request);
                             }
                             else{
                                 throw new NotFoundHttpException('Request is not found');
                             }
+                        },
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['create'],
+                        'roles' => ['@'],
+                        'matchCallback' => function ($rules, $action) {
+                            $currentUser = Yii::$app->user->identity;
+                            $requester_id = Yii::$app->request->getBodyParam('requester_id');
+                            $task_id = Yii::$app->request->getBodyParam('task_id');
+                            if ($currentUser->getId() == $requester_id) {
+                                return Yii::$app->requestService->canCreate($task_id, $requester_id);
+                            } else {
+                                throw new AccessDeniedException("Вы можете делать запрос только от своего имени!");
+                            }
+
                         },
                     ],
                     [
--- services/RequestService.php
+++ services/RequestService.php
@@ -10,12 +10,38 @@ namespace app\services;
 
 
 use app\models\Request;
+use app\models\Task;
+use yii\base\Component;
 use yii\web\IdentityInterface;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 
-class RequestService
+class RequestService extends Component
 {
-    public static function canDelete(IdentityInterface $user, Request $request)
+    /**
+     * @param IdentityInterface $user
+     * @param Request $request
+     * @return bool
+     */
+    public function canDelete(IdentityInterface $user, Request $request)
     {
         return $user->getId() == $request->requester_id;
+    }
+
+    /**
+     * @param $requester_id
+     * @param $task_id
+     * @return bool
+     */
+    public function canCreate($task_id, $requester_id)
+    {
+        $request = Request::find()->andWhere(['task_id' => $task_id, 'requester_id' => $requester_id])->exists();
+        if ($request) {
+            throw new AccessDeniedException("Вы уже сделали запрос на выполнение этой задачи!");
+        }
+        $task = Task::find()->andWhere(['id' => $task_id, 'owner_id' => $requester_id])->exists();
+        if ($task) {
+            throw new AccessDeniedException("Вы не можете делать запрос на выполнение своей задачи!");
+        }
+        return true;
     }
 }
