6590023d2980e532011fa44c0a53362822eb678c
IlyaKislitsin <ilya.kislitsin@gmail.com>
2018-12-09 22:14:04 +0500

    добавил переменные для новых статусов задачи

--- config/params.php
+++ config/params.php
@@ -6,4 +6,11 @@ return [
     'taskAtWorkStatusId' => 2,
     'taskCompletedStatusId' => 3,
     'taskCanceledStatusId' => 4,
+    'taskSentForReviewByTheWorkerStatusId' => 5,
+    'workerRefusedToPerformTheTask' => 6,
+    'taskSentForRevisionStatusId' => 7,
+    'taskMarkedForCanceledByTheOwnerStatusId' => 8,
+    'taskNotCompletedOnTimeStatusId' => 9,
+    'taskInDisputeStatusId' => 10,
+
 ];
